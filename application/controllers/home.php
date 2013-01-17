de8f50b33cea17429c5ff6b3d409422e4afed55f
Rachaelgit config --global user.name Rachaelgit config --global user.name Rachael <rachaelann.hill@gmail.com>
2013-01-17 18:34:45 -0500

    header view

---
+++ application/controllers/home.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+	
+	function __construct()
+	{
+		parent::__construct();
+	}
+		
+	public function index()
+	{
+  	//loading main header view
+		$this->load->view('header_view');
+	}
+}
---
+++ application/views/header_view.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<!--  ==================== COMMENTS ====================	
+  Author:  Rachael Hill
+  Project: Miss Know It All - Advice Blog
+  Date: 01/17/2013
+======================================================= -->	
+<head>
+  <meta charset="utf-8">
+  <title>Miss Know It All</title>
+  <meta name="description" content="Advice blog">
+  <meta name="author" content="Rachael">
+  <link rel="stylesheet" href="<? base_url();?>incs/css/screen.css" type="text/css" media="screen"/>
+  <!--[if lt IE 9]>
+  <![endif]-->
+</head>
+<body>
+<!--  ==================== HEADER ====================	-->	
+	<header>
+		<h1 class="logo"><a href="">Miss Know It All</a></h1>
+		<h2 class="create-post"><a href="">Ask Miss Know It All</a></h2>
+	</header>
