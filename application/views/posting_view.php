43972990e21bda02a30c47d1de33ecd68a7881bd
Rachaelgit config --global user.name Rachaelgit config --global user.name Rachael <rachaelann.hill@gmail.com>
2013-01-17 20:25:31 -0500

    connected db

--- application/views/posting_view.php
+++ application/views/posting_view.php
@@ -1,13 +1,13 @@
 <!--  ==================== POSTING CONTENT ====================	-->		
   <div class="wrapper">
-    <form class="post-form">
+    <?=form_open('home/', array('class' => 'post-form'));?>
   		<label>Title:</label>
   		<p><input type="text" name="titleInpt" class="title-inpt" placeholder="Ex: First Post"/></p>
   		<label>Dear Miss Know It All:</label>
   		<p><textarea name="postInpt" class="post-inpt"></textarea></p>
   		<label>Signature:</label>
   		<p><input type="text" name="signInpt" class="sign-inpt" placeholder="Ex: John Smith" /></p>
-  		<p class="submit-btn"><?=anchor('home/', 'Get Advice!')?></p>
+  		<p class="submit-btn"><input type="submit" name="submit" value="" /></p>
   		<p class="cancel-btn"><?=anchor('home/', 'Cancel')?></p>
     </form>
     <div class="reasons">
