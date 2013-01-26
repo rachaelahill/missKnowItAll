f1db3887f32ed6ebd36894268dc52766c311f14e
Rachaelgit config --global user.name Rachaelgit config --global user.name Rachael <rachaelann.hill@gmail.com>
2013-01-26 18:06:04 -0500

    admin controller loading admin_header_view

---
+++ application/controllers/admin.php
@@ -0,0 +1,23 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+	
+	function __construct()
+	{
+		parent::__construct();
+		//loads post model
+		$this->load->model('admin_model');
+	}
+	
+	public function index(){
+	  //loading admin header view
+	  $this->load->view('admin_header_view');
+	  
+	  //loading admin view
+	  $this->load->view('admin_login_view');
+	  
+  	//loading footer view
+		$this->load->view('footer_view');
+	}
+
+}
---
+++ application/views/admin_header_view.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<!--  ==================== INFORMATION ====================	
+  Author:  Rachael Hill
+  Project: Miss Know It All - Advice Blog
+  Date: 01/26/2013
+======================================================= -->	
+<head>
+  <meta charset="utf-8">
+  <title>Miss Know It All</title>
+  <meta name="description" content="Advice blog">
+  <meta name="author" content="Rachael">
+  <link rel="stylesheet" href="<?=base_url();?>incs/css/screen.css" type="text/css" media="screen"/>
+  <link rel="stylesheet" href="<?=base_url();?>incs/css/flexipage.css" type="text/css" media="screen"/>
+  <!-- FONTS -->
+  <link href='http://fonts.googleapis.com/css?family=Arizonia|Fanwood+Text' rel='stylesheet' type='text/css'>  
+  <!--[if lt IE 9]>
+  <![endif]-->
+</head>
+<body>
+<!--  ==================== HEADER ====================	-->	
+	<header>
+		<h1 class="logo-name"><?=anchor('admin/', 'Miss Know It All');?></h1>
+		<p class="logo-image">Bow</p>
+	</header>
