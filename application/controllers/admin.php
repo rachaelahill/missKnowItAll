eae2142a867f0cee974a16e2d70b1c9eda062029
Rachaelgit config --global user.name Rachaelgit config --global user.name Rachael <rachaelann.hill@gmail.com>
2013-01-26 18:50:42 -0500

    loading admin dashboard view to controller

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -13,10 +13,21 @@ class Admin extends CI_Controller {
 	  //loading admin header view
 	  $this->load->view('admin_header_view');
 	  
-	  //loading admin view
+	  //loading admin login view
 	  $this->load->view('admin_login_view');
 	  
   	//loading footer view
+		$this->load->view('footer_view');
+	}
+	
+	public function admin_dashboard(){
+  	//loading admin header view
+	  $this->load->view('admin_header_view');
+	  
+	  //loading admin dashboard view
+	  $this->load->view('admin_dashboard_view');
+	  
+	  //loading footer view
 		$this->load->view('footer_view');
 	}
 
---
+++ application/views/admin_dashboard_view.php
@@ -0,0 +1,5 @@
+<!--  ==================== ADMIN DASHBOARD ====================	-->		
+  <div class="wrapper">
+      <h1>ADMIN LOGGED IN</h1>
+  		
+  </div><!-- /wrapper -->
