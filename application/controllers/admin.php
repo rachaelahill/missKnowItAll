9c7c7785f0f5cd2e599969a436db6bcfefb1c6c2
Rachaelgit config --global user.name Rachaelgit config --global user.name Rachael <rachaelann.hill@gmail.com>
2013-01-29 17:51:40 -0500

    created admin_response_view and conntext to it in controller

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -12,7 +12,8 @@ class Admin extends CI_Controller {
 		//loads post model
 		$this->load->model('post_model');
 	}
-	
+
+//function runs once admin is logged in
 	public function index(){
 	  //setting variable to session userdata
 	  $admin['logged_in'] = $this->session->userdata('logged_in');
@@ -36,6 +37,27 @@ class Admin extends CI_Controller {
 		$this->load->view('footer_view');
 	}
 	
+//function runs when admin clicks on continue reading	
+	public function post_response()
+	{
+	  //gets users' posts from model
+	  $data = array();
+	  if($query = $this->admin_model->get_single_post())
+	  {
+  	  $data['posts'] = $query;
+	  }
+	  
+  	//loading admin header view
+  	$this->load->view('admin_header_view');
+  	
+  	//loading admin response view
+  	$this->load->view('admin_response_view', $data);
+  	
+  	//loading footer view
+		$this->load->view('footer_view');
+	}
+	
+//function runs when admin click log out	
 	public function log_out(){
 	  //removes session once admin clicks log out
   	$this->session->sess_destroy();
---
+++ application/views/admin_response_view.php
@@ -0,0 +1,37 @@
+<!--  ==================== ADMIN POST RESPONSE ====================	-->		
+	<div class="wrapper">
+  	<? if(isset($posts)) : foreach($posts as $row) :?>	
+  	  <div class="all-posts">
+    	   <h2 class="title-detail"><?=$row->titleName;?></h2>
+    	   <span class="ribbon-wrapper">
+        		<span class="ribbon-front">
+        			<p class="date-posted"><?=date("F j, Y", $row->postDate);?></p>
+        		</span>
+        		<span class="ribbon-edge-topleft"></span>
+        		<span class="ribbon-edge-back-topleft"></span>
+        		<span class="ribbon-edge-topright"></span>
+        		<span class="ribbon-edge-bottomleft"></span>
+        		<span class="ribbon-edge-bottomright"></span>
+        		<span class="ribbon-back-left"></span>
+        		<span class="ribbon-back-right"></span>
+        	</span><!-- /Ribbon -->
+        <div class="post-content">
+    	    <p class="dear-txt">Dear Miss Know It All,</p>
+    	    <p class="post-txt"><?=$row->postTxt;?></p>
+    	    <p class="user-sign"><?=$row->userSign;?></p>
+        </div><!-- /post-content -->
+      </div><!-- /all-posts -->
+      <div class="tucked-corners top-corners">
+        <span class="tucked-corners bottom-corners">       
+          <h2>Re: <?=$row->titleName;?></h2>
+    	    <p class="resp-date"></p>
+    	    <div class="post-content">
+      	    <p class="dear-txt">Dear <?=$row->userSign;?>,</p>
+      	    <p class="post-txt"></p>
+        	    <p class="sign-resp">Miss Know It All</p>
+    	    </div><!-- /post-content -->
+        </span><!-- /tucked-corners bottom-corners -->
+      </div><!-- /tucked-corners top-corners -->	
+  	<? endforeach ?>
+  	<? endif ?>
+	</div>
