e55c0c36233e0367db399bfa97c25410fa068739
Rachaelgit config --global user.name Rachaelgit config --global user.name Rachael <rachaelann.hill@gmail.com>
2013-01-30 16:28:08 -0500

    updated jquery in footer

--- application/views/footer_view.php
+++ application/views/footer_view.php
@@ -1,5 +1,5 @@
 <!--  ==================== FOOTER ====================	-->	
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.0.min.js"></script>
     <script type="text/javascript" src="<?=base_url();?>incs/js/jquery.flexipage.min.js"></script>
     <script type="text/javascript" src="<?=base_url();?>incs/js/jquery.truncatable.js"></script>
 		<script type="text/javascript" src="<?=base_url();?>incs/js/main.js"></script>
