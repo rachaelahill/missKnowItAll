c0a0028875fdb6eb58b06576fdc01f740eb03f29
Rachaelgit config --global user.name Rachaelgit config --global user.name Rachael <rachaelann.hill@gmail.com>
2013-02-01 10:28:36 -0500

    textarea min length 10

--- application/views/posting_view.php
+++ application/views/posting_view.php
@@ -12,7 +12,7 @@
   		<label>Title:</label>
   		<p><input type="text" name="titleInpt" data-validate="validate(required, maxlength(20))" class="post-inpt" placeholder="Ex: First Post"/></p>
   		<label>Dear Miss Know It All:</label>
-  		<p><textarea name="postInpt" data-validate="validate(required, maxlength(50))"  required="true" id="post"></textarea></p>
+  		<p><textarea name="postInpt" data-validate="validate(minlength(10))"  required="true" id="post"></textarea></p>
   		<label>Signature:</label>
   		<p><input type="text" name="signInpt" data-validate="validate(required, maxlength(50)" class="post-inpt" placeholder="Ex: John Smith" /></p>
   		<p><input type="submit" class="submit-btn" value="Get Advice!" /></p>
