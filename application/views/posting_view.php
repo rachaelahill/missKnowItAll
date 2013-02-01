f1e1b861ff0cae3e7188c8efb1879e7638073525
Rachaelgit config --global user.name Rachaelgit config --global user.name Rachael <rachaelann.hill@gmail.com>
2013-02-01 11:59:04 -0500

    ketchup fix

--- application/views/posting_view.php
+++ application/views/posting_view.php
@@ -10,11 +10,11 @@
     </div><!-- /reasons -->
     <?=form_open('client/create_post', array('class' => 'post-form'));?>
   		<label>Title:</label>
-  		<p><input type="text" name="titleInpt" data-validate="validate(required, maxlength(20))" class="post-inpt" placeholder="Ex: First Post"/></p>
+  		<p><input type="text" name="titleInpt" data-validate="validate(required)" class="post-inpt" placeholder="Ex: First Post"/></p>
   		<label>Dear Miss Know It All:</label>
   		<p><textarea name="postInpt" data-validate="validate(minlength(10))"  required="true" id="post"></textarea></p>
   		<label>Signature:</label>
-  		<p><input type="text" name="signInpt" data-validate="validate(required, maxlength(50)" class="post-inpt" placeholder="Ex: John Smith" /></p>
+  		<p><input type="text" name="signInpt" data-validate="validate(required)" class="post-inpt" placeholder="Ex: John Smith" /></p>
   		<input type="hidden" name="noResp" value="no"/>
   		<p><input type="submit" class="submit-btn" value="Get Advice!" /></p>
   		<p class="cancel-btn"><?=anchor('client/', 'Cancel')?></p>
